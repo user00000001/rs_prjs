168bb2bde3de6660dcadb52110525b9fddbc5f56
user00000001 <jaybrownjyb@gmail.com>
2022-02-17 21:59:42 +0800

    reference and borrow.
    Signed-off-by: user00000001 <jaybrownjyb@gmail.com>

--- rs012/src/main.rs
+++ rs012/src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("Hello, ");
+    let (s1, len) = calculate_string_length(&mut s);
+    println!("{:#}\n{}", s1, len);
+    {
+        let r1 = s1;
+        println!("{}", r1);
+    }
+    // let r2 = s1;
+    let r2 = &mut s;
+    println!("{}", r2);
+    let mut h = String::from("hello");
+    let r1 = &h;
+    let r2 = &h;
+    println!("{} {}", r1, r2);
+    let r3 = &mut h;
+    println!("{}", r3);
+    // let reference_to_nothing = dangle();
 }
+
+fn calculate_string_length(s: &mut String) -> (&mut String, usize) {
+    let length = s.len();
+    s.push_str("world!");
+    (s, length)
+}
+
+// fn dangle() -> &String {
+//     let s = String::from("Hello");
+//     &s
+// }
