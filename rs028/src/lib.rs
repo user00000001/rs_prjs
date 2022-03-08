2399bceba62e007956f903689eb76680644c8d42
user00000001 <jaybrownjyb@gmail.com>
2022-03-08 16:17:53 +0800

    iterator & closure
    Signed-off-by: user00000001 <jaybrownjyb@gmail.com>

---
+++ rs028/src/lib.rs
@@ -0,0 +1,168 @@
+use std::fs;
+use std::env;
+use std::error::Error;
+
+#[derive(PartialEq, Debug)]
+pub struct Shoe {
+    size: u32,
+    style: String,
+}
+
+#[allow(dead_code)]
+pub fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
+
+pub struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    #[allow(dead_code)]
+    pub fn new() -> Counter {
+        Counter {
+            count: 0
+        }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+pub struct Config {
+    pub query: String, 
+    pub filename: String,
+    pub case_insensitive: bool,
+}
+
+impl Config {
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        args.next();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+        let case_insensitive = env::var("CASE_INSENSITIVE").is_err();
+        println!("Now we query `{}` at {}\n", query, filename);
+        Ok(Config {
+            query,
+            filename,
+            case_insensitive,
+        })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let result = fs::read_to_string(config.filename);
+    match result {
+        Ok(contents) => {
+            if config.case_insensitive {
+                for line in search(&config.query, &contents) {
+                    println!("{}", line);
+                }
+            } else {
+                for line in search_case_insensitive(&config.query, &contents) {
+                    println!("{}", line);
+                }
+            }
+
+            Ok(())
+        }
+        Err(err) => Err(Box::new(err))
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents.lines()
+        .filter(|line| line.contains(query) )
+        .collect()
+}
+
+pub fn search_case_insensitive<'a>(
+    query: &str,
+    contents: &'a str,
+) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn filters_by_size() {
+        let shoes = vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker"),
+            },
+            Shoe {
+                size: 13,
+                style: String::from("sandal"),
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot"),
+            },
+        ];
+        let in_my_size = shoes_in_size(shoes, 10);
+
+        assert_eq!(
+            in_my_size,
+            vec![
+                Shoe {
+                    size: 10,
+                    style: String::from("sneaker"),
+                },
+                Shoe {
+                    size: 10,
+                    style: String::from("boot"),
+                },
+            ]
+        );
+    }
+
+    #[test]
+    fn calling_next_directly() {
+        let mut counter = Counter::new();
+
+        assert_eq!(counter.next(), Some(1));
+        assert_eq!(counter.next(), Some(2));
+        assert_eq!(counter.next(), Some(3));
+        assert_eq!(counter.next(), Some(4));
+        assert_eq!(counter.next(), Some(5));
+        assert_eq!(counter.next(), None);
+    }
+
+    #[test]
+    fn using_other_iterator_trait_methods() {
+        let sum: u32 = Counter::new()
+            .zip(Counter::new().skip(1))
+            .map(|(a, b)| a * b)
+            .filter(|x| x % 3 == 0 )
+            .sum();
+        assert_eq!(18, sum);
+    }
+}
