31f592342cf58e2072c894db62bb5bcb81af6f2c
user00000001 <jaybrownjyb@gmail.com>
2022-03-28 15:54:34 +0800

    multithreaded server's graceful shutdown and cleanup
    Signed-off-by: user00000001 <jaybrownjyb@gmail.com>

---
+++ rs042/src/main.rs
@@ -0,0 +1,49 @@
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+use std::fs;
+use std::thread;
+use std::time::Duration;
+
+use thread_pool::ThreadPool;
+
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    // for stream in listener.incoming() {
+    //     let stream = stream.unwrap();
+    //     handle_connection(stream);
+    // }
+    let pool = ThreadPool::new(4);
+    for stream in listener.incoming().take(3) { // echo -ne "GET /sleep HTTP/1.1\r\n" | nc -v 127.0.0.1 7878 & echo -ne "GET / HTTP/1.1\r\n" | nc -v 127.0.0.1 7878
+        let stream = stream.unwrap();
+        pool.execute(||{
+            handle_connection(stream);
+        });
+    }
+    println!("Shutting down.");
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0;1024];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT Found", "404.html")
+    };
+    let contents = fs::read_to_string(filename).unwrap();
+    let rsp = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+    stream.write(rsp.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
